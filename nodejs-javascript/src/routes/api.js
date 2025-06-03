9e45e61711b34d76539b294aff2ad515d26cfb93
NguyenTanTai <ngtantai48@gmail.com>
2025-06-03 21:00:52 +0700

    - delay middleware, split authorization string from header to get token

--- nodejs-javascript/src/routes/api.js
+++ nodejs-javascript/src/routes/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const { createUser, handleLogin, getUser } = require('../controllers/userController');
+const delay = require("../middleware/delay");
 
 const routerAPI = express.Router();
+
+routerAPI.get(/(.*)/, delay);
 
 // routerAPI.get("/", (req, res) => {
 //     return res.status(200).json("Hello world API")
