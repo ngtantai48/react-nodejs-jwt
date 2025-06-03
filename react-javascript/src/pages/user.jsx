f4eece354fa7c04218a992f5fe20b3cbaccaad31
NguyenTanTai <ngtantai48@gmail.com>
2025-06-04 04:21:57 +0700

    - Done: call API with access_token and use hook useContext to share data between components.

--- react-javascript/src/pages/user.jsx
+++ react-javascript/src/pages/user.jsx
@@ -1,4 +1,4 @@
-import { Table } from 'antd';
+import { notification, Table } from 'antd';
 import { useEffect, useState } from 'react';
 import { getUserApi } from '../utils/api';
 
@@ -8,8 +8,13 @@ const UserPage = () => {
     useEffect(() => {
         const fetchUser = async () => {
             const res = await getUserApi();
-            if (res) {
+            if (!res?.message) {
                 setDataSource(res)
+            } else {
+                notification.error({
+                    message: "Unauthorized",
+                    description: res.message
+                })
             }
         }
         fetchUser();
@@ -30,14 +35,13 @@ const UserPage = () => {
         },
     ];
 
-
     return (
         <div style={{ padding: '100px' }}>
             <Table
                 bordered
                 dataSource={dataSource} columns={columns}
                 rowKey={"_id"}
-            />;
+            />
         </div>
     )
 }
